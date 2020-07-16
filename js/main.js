9e8609858a07b9c4f48897d7942639a6a9864e16
Marcos Vinicius <viniciusdejesus565@gmail.com>
2020-07-16 00:59:18 -0300

    Finalizando projeto

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Menu em DropDown
+
+$(function () {
+       $('.dropdown-toggle').dropdown()
+})
+
+/*-------------------------------------------------*/
+
+// Menu Scroll
+
+$(document).ready(function () {
+
+       var _containerLink = $('[data-container="link"]')
+       var _containerMenu = $('[data-container="menu"]')
+
+       // Evento Scroll no menu
+       _containerLink.click(function () {
+
+              var _sessao = $(this).data('link')
+
+              function posicao(Seletor) {
+
+                     var _alturaMenu = parseInt(_containerMenu.outerHeight())
+                     var _posicao = parseInt(($('[data-container="' + Seletor + '"]').position().top) - _alturaMenu)
+
+                     return _posicao
+              }
+
+              // Evento Animação
+              $('html, body').animate({ scrollTop: posicao(_sessao) }, 1300)
+
+              return false
+       })
+})
